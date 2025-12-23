0897728bcdd8d76dd6485bb4442c5eae99f0511c
Satendra Kumar <153271655+Satendra112@users.noreply.github.com>
2025-12-23 10:02:33 +0530

    D:\portfolio
    code files are replaced in the folder

---
+++ script.js
@@ -0,0 +1,51 @@
+function openMenu(){
+  document.querySelector(".dropdown").style.transform = "translateY(0)";
+}
+
+function closeMenu(){
+  document.querySelector(".dropdown").style.transform = "translateY(-100%)";
+}
+
+/* TYPEWRITER */
+const texts = [
+  "Entry-Level Data Analyst",
+  "Excel & Power BI Specialist",
+  "SQL Learner"
+];
+
+let index = 0;
+let char = 0;
+const speed = 100;
+const target = document.querySelector(".typewriter-text");
+
+function type(){
+  if(char < texts[index].length){
+    target.innerHTML += texts[index].charAt(char);
+    char++;
+    setTimeout(type, speed);
+  }else{
+    setTimeout(erase, 1500);
+  }
+}
+
+function erase(){
+  if(char > 0){
+    target.innerHTML = texts[index].substring(0, char - 1);
+    char--;
+    setTimeout(erase, 50);
+  }else{
+    index = (index + 1) % texts.length;
+    setTimeout(type, 500);
+  }
+}
+
+window.onload = type;
+/* SKILL BAR ANIMATION ON LOAD */
+window.addEventListener("load", () => {
+  const skillBars = document.querySelectorAll(".skill-fill");
+
+  skillBars.forEach(bar => {
+    const percent = bar.getAttribute("data-percent");
+    bar.style.width = percent;
+  });
+});
